9d6de699012c88a99275fd50dd04093616d1382e
rajali <rajali1432@gmail.com>
2024-06-03 11:49:59 +0530

    replace the cors origin with deploy frontend url

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 4000
 dotenv.config();
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://levitec-mart.vercel.app"],
     credentials: true
 }))
 
